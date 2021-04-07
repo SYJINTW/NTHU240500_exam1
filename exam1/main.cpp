4e7c5def94e8a7e29552fb912ebb311395c1ab20
SYJINTW <yuanjiun911@outlook.com>
2021-04-07 18:11:36 +0800

    without freq

--- exam1/main.cpp
+++ exam1/main.cpp
@@ -5,7 +5,7 @@
 #include <thread> // for thread
 #include <ctime> // for clock
 
-using namespace std::chrono; // to set nanosecondsd
+using namespace std::chrono; // to set nanosecond
 
 Timer t;
 Thread changeThread;
@@ -16,6 +16,7 @@ EventQueue queue(32 * EVENTS_EVENT_SIZE);
 InterruptIn upbn(D13);
 InterruptIn downbn(D11);
 InterruptIn selectbn(D12);
+InterruptIn samplebn(USER_BUTTON);
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 AnalogOut aout(PA_4);
@@ -29,33 +30,26 @@ int pos = 0;
 
 // setting
 double T = 1/freq;
-int range = (int)(8000*freq);
-int flat = 24000 - 2*range;
+int range = (int)(800*freq);
+int flat = 2400 - 2*range;
 double amp = 3/3.3;
-double waitTime[4] = {1,2,3,4};
-    
+double rangef = (double)range*amp;
 
-//float ADCdata[500]; // record sample data
+float ADCdata[500]; // record sample data
 
-
-/*
+// ADC
 void sample(void)
 {
-    //t.start();
     for(int i = 0; i < 500; i++)
     {
-        ADCdata[i] = ain;
-        ThisThread::sleep_for(2000ms/500);
+        ADCdata[i] = aout;
+        ThisThread::sleep_for(4ms);
     }
-    //t.stop();
     for(int i = 0; i < 500; i++)
     {
         printf("%f\r\n", ADCdata[i]);
     }
-    //auto s = chrono::duration_cast<chrono::seconds>(t.elapsed_time()).count();
-    //printf("time: %llus\n", s); // -> 3 sec & 500
 }
-*/
 
 // show up
 void U()
@@ -86,30 +80,19 @@ void ST()
     uLCD.locate(0,0);
     uLCD.printf("%-6.3f\n", freqlist[pos]);
     ThisThread::sleep_for(100ms);
-    range = (int)(8000*freq);
-    flat = 24000-2*range;
+    range = (int)(800*freq);
+    flat = 2400-2*range;
+    rangef = (double)range*amp;
 }
 
 
 int main()
 {    
-    // check if the button works
-    /*
-    while(1)
-    {
-        printf("up has value : %d \n\r", upbn.read());
-        printf("down has value : %d \n\r", downbn.read());
-        printf("select has value : %d \n\r", selectbn.read());
-        ThisThread::sleep_for(250ms);
-    }
-    */
-
     // uLCD first show
     uLCD.color(RED);
     uLCD.locate(0,0);
     uLCD.printf("%-6.3f\n", freqlist[pos]);
     ThisThread::sleep_for(100ms);
-
 
     // start queue thread
     changeThread.start(callback(&queue, &EventQueue::dispatch_forever));
@@ -118,35 +101,27 @@ int main()
     upbn.rise(queue.event(&U));
     downbn.rise(queue.event(&DN));
     selectbn.rise(queue.event(&ST));
+    samplebn.rise(queue.event(&sample));
     
     // DAC
-    
-    // start thread for sample
-    // thread.start(sample);
-
     // main loop
     while(1)
     {
         for(int i = 0; i < range; i++)
         {
-            aout = i/(float)range*amp;
-            wait_us(10);
+            aout = i/rangef;
+            wait_us(40);
         }
         for(int count = 0; count < flat; count++)
         {
-            aout = range/(float)range*amp;
-            wait_us(10);
+            aout = range/rangef;
+            wait_us(40);
         }
         for(int i = range; i > 0; i--)
         {
-            aout = i/(float)range*amp;
-            wait_us(10);
+            aout = i/rangef;
+            wait_us(40);
         }
-        
-        //auto s = chrono::duration_cast<chrono::milliseconds>(t.elapsed_time()).count();
-        //printf("time: %llus\n", s); // -> 3 sec & 500
-        //ThisThread::sleep_for(5s);
-
     }
     
 }
